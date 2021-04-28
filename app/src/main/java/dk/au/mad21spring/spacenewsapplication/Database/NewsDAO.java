3485c84b79bb25fcb3830e533d2a65fa7a0d59a5
Dikte Straadt <au611785@uni.au.dk>
2021-04-29 00:06:37 +0200

    Flere API ting

--- app/src/main/java/dk/au/mad21spring/spacenewsapplication/Database/NewsDAO.java
+++ app/src/main/java/dk/au/mad21spring/spacenewsapplication/Database/NewsDAO.java
@@ -1,8 +1,12 @@
 package dk.au.mad21spring.spacenewsapplication.Database;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Dao
 public interface NewsDAO {
@@ -12,4 +16,10 @@ public interface NewsDAO {
 
     @Query("DELETE FROM Article WHERE ArticleID LIKE :ArticleID")
     void deleteArticle(String ArticleID);
+
+    @Query("SELECT * FROM Article")
+    LiveData<List<Article>> getAllReadLaterArticles();
+
+    @Query("SELECT * FROM Article")
+    List<Article> getAllReadLaterArticlesNonAsync();
 }
--- app/src/main/java/dk/au/mad21spring/spacenewsapplication/Database/Repository.java
+++ app/src/main/java/dk/au/mad21spring/spacenewsapplication/Database/Repository.java
@@ -3,6 +3,8 @@ package dk.au.mad21spring.spacenewsapplication.Database;
 import android.app.Application;
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,6 +14,8 @@ import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -23,8 +27,10 @@ public class Repository {
     private ExecutorService executor;
     private RequestQueue queue;
     private Application app;
+    private int index = 0;
 
     //private LiveData<List<Article>> readLaterList;
+    private List<Article> readLaterList;
 
     public static Repository getInstance(Application app) {
         if (repository == null) {
@@ -37,13 +43,31 @@ public class Repository {
         db = NewsDatabase.getDatabase(app.getApplicationContext());
         executor = Executors.newSingleThreadExecutor();
         this.app = app;
+        //readLaterList = db.newsDAO().getAllReadLaterArticles();
+        readLaterList = db.newsDAO().getAllReadLaterArticlesNonAsync();
     }
 
     //public LiveData<List<Article>> getReadLaterList() {return readLaterList;}
+    public List<Article> getReadLaterList() {return readLaterList;}
+
+    public Article getReadLaterArticle(){
+
+        int maxIndex = readLaterList.size();
+        Article article = readLaterList.get(index);
+
+        if (index == maxIndex-1){
+            index = 0;
+        }
+        else{
+            index++;
+        }
+
+        return article;
+    }
 
     // ########################## API methods ##########################
 
-    public void sendRequest(String url){
+    public void sendRequestOneArticle(String url){
         if(queue==null){
             queue = Volley.newRequestQueue(app.getApplicationContext());
         }
@@ -53,7 +77,7 @@ public class Repository {
                     @Override
                     public void onResponse(String response) {
                         Log.d(TAG, "API accessed successfully! " + response);
-                        parseJson(response);
+                        parseJsonOneArticle(response);
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -65,15 +89,50 @@ public class Repository {
         queue.add(stringRequest);
     }
 
-    private void parseJson(String json){
+    public void sendRequestAllArticles(){
+        String url = "https://spaceflightnewsapi.net/api/v2/articles";
+
+        if(queue==null){
+            queue = Volley.newRequestQueue(app.getApplicationContext());
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "API accessed successfully! " + response);
+                        parseJsonAllArticles(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error occured while trying to access API!", error);
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+    private void parseJsonOneArticle(String json){
         Gson gson = new GsonBuilder().create();
         ArticleDTO articleFromAPI =  gson.fromJson(json, ArticleDTO.class);
 
         if(articleFromAPI!=null){
             Log.d(TAG, "parseJson: Title: " + articleFromAPI.getTitle() + ", news site: " + articleFromAPI.getNewsSite());
 
-            addArticleAsynch(new Article("", articleFromAPI.getTitle(), articleFromAPI.getUrl(), articleFromAPI.getImageUrl(), articleFromAPI.getNewsSite(),articleFromAPI.getSummary()));
+            addArticleAsynch(new Article("", articleFromAPI.getTitle(), articleFromAPI.getUrl(), articleFromAPI.getImageUrl(), articleFromAPI.getNewsSite(),articleFromAPI.getSummary(), articleFromAPI.getPublishedAt(), articleFromAPI.getUpdatedAt()));
         }
+    }
+
+    private void parseJsonAllArticles(String json){
+//        Gson gson = new GsonBuilder().create();
+//        ArticleDTO articleFromAPI =  gson.fromJson(json, ArticleDTO.class);
+//
+//        if(articleFromAPI!=null){
+//            Log.d(TAG, "parseJson: Title: " + articleFromAPI.getTitle() + ", news site: " + articleFromAPI.getNewsSite());
+//
+//            addArticleAsynch(new Article("", articleFromAPI.getTitle(), articleFromAPI.getUrl(), articleFromAPI.getImageUrl(), articleFromAPI.getNewsSite(),articleFromAPI.getSummary(), articleFromAPI.getPublishedAt(), articleFromAPI.getUpdatedAt()));
+//        }
     }
 
     // ########################## Asynch methods ##########################
@@ -97,11 +156,5 @@ public class Repository {
             }
         });
     }
-
-
-
-
-
-
 }
 
